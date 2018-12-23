d70b4850952fe75df145011eed1be0b57646e5a2
Robin Tram <robin.tram@gmail.com>
2018-12-23 12:16:50 -0500

    Sunday Noon Commit

--- app/serializers/transaction_serializer.rb
+++ app/serializers/transaction_serializer.rb
@@ -1,5 +1,5 @@
 class TransactionSerializer < ActiveModel::Serializer
-  attributes :id, :date, :description, :amount, :category
+  attributes :id, :date, :description, :amount, :category, :user
 
   belongs_to :category
   belongs_to :user
