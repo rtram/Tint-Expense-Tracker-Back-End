01a3cf92fae5518d11f774a6e87e837c0d9d2e70
Robin Tram <robin.tram@gmail.com>
2018-12-24 14:46:10 -0500

    Added Create Action

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,22 @@
 class TransactionsController < ApplicationController
+  def index
+    @transactions = Transaction.all
+    render json:@transactions
+  end
+
   def show
     @transaction = Transaction.find(params[:id])
     render json: @transaction
+  end
+
+  def create
+    @transaction = Transaction.new
+    @transaction.date = params[:date]
+    @transaction.description = params[:description]
+    @transaction.user_id = params[:user][:id]
+    @transaction.category_id = params[:category][:id]
+    @transaction.amount = sprintf("%2.2f", params[:amount])
+
+    @transaction.save
   end
 end
